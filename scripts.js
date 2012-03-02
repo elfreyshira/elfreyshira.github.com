460f3457ec080b7dcfa3df00577fdac4af80b834
Elfrey Shira <elfreyshira@gmail.com>
2012-03-02 10:18:04 -0800

    added transition for bg boxes

--- scripts.js
+++ scripts.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
     function() {
       var col = $(this).attr("class").split(" ")[1];
       var change_class = ".bg."+col;
-      $(change_class).css("opacity","0.8");
+      $(change_class).stop().animate({opacity:0.8}, 150);
     },
     function() {
       var col = $(this).attr("class").split(" ")[1];
       var change_class = ".bg."+col;
-      $(change_class).css("opacity","0.4");
+      $(change_class).stop().animate({opacity:0.4}, 150);
     });
 });
