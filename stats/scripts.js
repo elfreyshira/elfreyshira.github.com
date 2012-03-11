9f33574c27000f347c129619bc460f24a41149bf
Elfrey Shira <elfreyshira@gmail.com>
2012-03-10 22:39:32 -0600

    history is now working

--- stats/scripts.js
+++ stats/scripts.js
@@ -1,7 +1,20 @@
-function changeStats(){
-  var str = $("#input_text").val();
+history_count = 0;
 
-  
+function addStats() {
+  var str = $("#input_text").val();
+  changeStats(str, "add");
+}
+
+function deleteStats(str, id_count) {
+  $("#"+id_count).remove();
+  changeStats(str, "delete");
+}
+
+function changeStats(str, change){
+
+  // set the multiplier (m) depending on whether it's adding or deleting an action
+  var m  = (change=="add") ? 1 : -1;
+
   var str_split = str.split(" ",2);
   if (str_split.length != 2){
     return false
@@ -10,45 +23,59 @@ function changeStats(){
   var action   = str_split[1].toLowerCase();
 
   var arr = [0,0,0,0,0,0,0,0,0,0,0];
-  if      (action[0]=="m"){ arr[2] += 1; }    // miss
-  else if (action[0]=="b"){ arr[7] += 1; }    // block
-  else if (action[0]=="t"){ arr[9] += 1; }    // turnover
-  else if (action[0]=="r"){ arr[6] += 1; }    // rebound
-  else if (action[0]=="s"){ arr[8] += 1; }    // steal
-  else if (action[0]=="a"){ arr[5] += 1; }    // assists
-  else if (action=="foul"){ arr[10] += 1; }//fouls
+  if      (action[0]=="m"){ arr[2] += m; action = "missed a shot";}    // miss
+  else if (action[0]=="b"){ arr[7] += m; action = "blocked a shot";}    // block
+  else if (action[0]=="t"){ arr[9] += m; action = "turned it over";}    // turnover
+  else if (action[0]=="r"){ arr[6] += m; action = "rebounded";}    // rebound
+  else if (action[0]=="s"){ arr[8] += m; action = "got a steal";}    // steal
+  else if (action[0]=="a"){ arr[5] += m; action = "got an assist";}    // assists
+  else if (action=="foul"){ arr[10] += m; }//fouls
 
   else if (action == "2" || action == "3"){
-    arr[0] += parseInt(action);
-    arr[1] += 1;
-    arr[2] += 1;
+    arr[0] += m*parseInt(action);
+    arr[1] += m*1;
+    arr[2] += m*1;
+    action = "made a "+action;
   }
   else if (action == "ft"){
-    arr[0] += 1;
-    arr[3] += 1;
-    arr[4] += 1;
+    arr[0] += m;
+    arr[3] += m;
+    arr[4] += m;
+    action = "made a free throw"
   }
   else if (action == "ftmiss"){
-    arr[4] += 1;
+    arr[4] += m;
+    action = "missed a free throw"
   }
-  else {
+  else if (change=="add"){
     // if it's an incorrect input, then put the name back, but don't select it. it's assuming it got the thing wrong
-    $("#input_text").val(name+" ");
+    $("#input_text").val(name+" ").select();
     return false;
   }
 
-  // put the name back into the input text so that it's easy to go to the next action if it's the same person
-  $("#input_text").val(name+" ").select();
 
-  // updates the latest action
-  $("#latest").animate({opacity: 0.0}, "fast",
-    function() {
-      $(this).html(name+" "+action).animate({opacity: 1.0}, "fast");
-    });
-  // update history
-  $("#history_list").prepend("<li>"+str+"</li>");
+  if (change=="delete") {
+    $("#input_text").val(str).select();
+  }
+
+  if (change=="add") {
+    // put the name back into the input text so that it's easy to go to the next action if it's the same person
+    $("#input_text").val(name+" ").select();
+
+    // updates the latest action
+    $("#latest").animate({opacity: 0.0}, "fast",
+      function() {
+        $(this).html(name+" "+action).animate({opacity: 1.0}, "fast");
+      });
+    // update history
+    var onclick_str = "onClick = \"deleteStats('"+str+"'"+", "+history_count+"); return false;\"";
+    $("#history_list").prepend(
+      "<li id='"+history_count+"'><a href='' "+onclick_str+"><img src='delete.png'/></a>"+name+" "+action+"</li>");
+    history_count += 1;
+  }
 
 
+  // update the stat table
   var rows = ["name","points","shots_made","shots_attempted","ft_made","ft_attempted","assists","rebounds",
               "blocks","steals","turnovers", "fouls"];
 
