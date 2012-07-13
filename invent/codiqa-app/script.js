00cd0ab3e0d660e5d2d2aa575b01b70147769807
Elfrey Shira <elfreyshira@gmail.com>
2012-07-12 21:08:12 -0500

    add questions

--- invent/codiqa-app/script.js
+++ invent/codiqa-app/script.js
@@ -1,3 +1,7 @@
+jQuery.prototype.re = function() {
+  this.trigger('create');
+}
+
 $(document).ready(function() {
 
   // Hide anything that should be hidden
@@ -5,18 +9,30 @@ $(document).ready(function() {
 
   // Code that reacts to any click that has a "data-activate" attribute
   $("[data-activate]").click( function() {
-    var id_shown = $(this).attr("data-activate");
-    var activate_type = $(this).attr("data-activate-type");
-    var element_to_show = $("#"+id_shown);
+    var activated = $(this);
+    var id_shown = activated.attr("data-activate");
+    var activate_type = activated.attr("data-activate-type");
+    var element_to_activate = $("#"+id_shown);
     switch(activate_type) {
       case "show":
-        element_to_show.slideDown("fast");
+        element_to_activate.slideDown("fast");
         break;
       case "hide":
-        element_to_show.slideUp("fast");
+        element_to_activate.slideUp("fast");
         break;
+      case "add-question":
+        var question_text = $("#new-question-textarea").val()
+        var data = {snippet: question_text,
+                        text: question_text};
+        
+        var html = Mustache.to_html(new_question_template, data);
+        // alert(html);
+        element_to_activate.append(html).re();
+
     }
   });
+
+  // var add_question = function()
 
   // Code that reacts to a submit on an input text
   $("[data-onsubmit]").click( function() {
@@ -39,7 +55,52 @@ $(document).ready(function() {
     return false;
   });
 
+  // Clear values that should be cleared at the beginning
   $("[data-value-clear=true]").val("");
+
+
+  var new_question_template = '<div data-role="collapsible" data-collapsed="false"><h3>{{snippet}}</h3><div><p>{{text}}</p>\
+                          <div data-role="fieldcontain">\
+                            <fieldset data-role="controlgroup" data-type="horizontal" data-mini="true">\
+                                <input name="radiobuttons5" id="radio12" value="radio1" type="radio" />\
+                                <label for="radio12">\
+                                    1\
+                                </label>\
+                                <input name="radiobuttons5" id="radio13" value="radio13" type="radio" />\
+                                <label for="radio13">\
+                                    2\
+                                </label>\
+                                <input name="radiobuttons5" id="radio14" value="radio14" type="radio" />\
+                                <label for="radio14">\
+                                    3\
+                                </label>\
+                                <input name="radiobuttons5" id="radio15" value="radio15" type="radio" />\
+                                <label for="radio15">\
+                                    4\
+                                </label>\
+                                <input name="radiobuttons5" id="radio16" value="radio16" type="radio" />\
+                                <label for="radio16">\
+                                    5\
+                                </label>\
+                            </fieldset>\
+                        </div></div></div>';
+  // var question = 
+  // var test = $('<div data-role="collapsible" data-collapsed="false"> \
+  //                       <h3>\
+  //                           HAHA\
+  //                       </h3>\
+  //                       <div>\
+  //                           <p>\
+  //                               Communication skills?\
+  //                           </p>\
+  //                       </div>\
+  //                   </div>');
+
+  // $("[data-role=collapsible-set]").append(test).trigger("create");
+
+  // populate_css(jQuery);
+
+
 
 
 });
